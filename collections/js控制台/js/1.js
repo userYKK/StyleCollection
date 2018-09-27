f718045a4d36772364cfc84e950a172b856857e6
ykk <yangkk@spsp-it.com>
2018-09-27 15:03:46 +0800

    完成 js  控制台效果的总结； 完成一个鼠标特效，另外一个需要进行下载

---
+++ collections/js控制台/js/1.js
@@ -0,0 +1,11 @@
+/**
+ * @file: 1.js
+ * @time: 2018/9/27 上午11:17
+ * @author: ykk
+ * @contact: yangkk@spsp-it.com
+ * @desc:
+ */
+
+console.log("%c \u5b89\u5168\u8b66\u544a\uff01", "font-size:50px;color:red;-webkit-text-fill-color:red;-webkit-text-stroke: 1px black;");
+
+console.log("%c \u6b64\u6d4f\u89c8\u5668\u529f\u80fd\u4e13\u4f9b\u5f00\u53d1\u8005\u4f7f\u7528\u3002\u8bf7\u4e0d\u8981\u5728\u6b64\u7c98\u8d34\u6267\u884c\u4efb\u4f55\u5185\u5bb9\uff0c\u8fd9\u53ef\u80fd\u4f1a\u5bfc\u81f4\u60a8\u7684\u8d26\u6237\u53d7\u5230\u653b\u51fb\uff0c\u7ed9\u60a8\u5e26\u6765\u635f\u5931 \uff01", "font-size: 20px;color:#333");
